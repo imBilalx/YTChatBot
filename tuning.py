bdfe0e46f7c9df50f9eac8a33e10c1878f885bac
ibilalakhtar <ibilalakhtar@icloud.com>
2023-12-17 16:45:41 -0500

    added requirements

--- tuning.py
+++ tuning.py
@@ -7,6 +7,7 @@ temperatures = [0.8, 1, 1.2]
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+
 def assistant_message(prompt, temperature, append: bool = False, messages=None):
     if messages is None:
         messages = []
@@ -29,18 +30,20 @@ def assistant_message(prompt, temperature, append: bool = False, messages=None):
 
     return response.choices[0].message.content, messages
 
+
 summary_storage = []
 
 for run in range(5):
-    print(f"-------Start of Run {run+1}/{5}-------")
+    print(f"-------Start of Run {run + 1}/{5}-------")
     for temp in temperatures:
         chunks = split_text_into_chunks(text, 3277)
         total_chunks = len(chunks) - 1
         messages = []
         for i, chunk in enumerate(chunks):
-            chunk_resp, messages = assistant_message(summarize_each_chunk(f"Raw Transcript {i}/{total_chunks}:\n{chunk}"),
-                                                     temp,
-                                                     messages=messages)
+            chunk_resp, messages = assistant_message(
+                summarize_each_chunk(f"Raw Transcript {i}/{total_chunks}:\n{chunk}"),
+                temp,
+                messages=messages)
             messages.append({"role": "system", "content": f"Transcript Part {i}/{total_chunks} Summary:\n{chunk_resp}"})
             if i == 5:
                 summary_storage.append(f"Transcript Part {i}/{total_chunks} Summary:\n{chunk_resp}")
@@ -53,7 +56,7 @@ for run in range(5):
             # Check for specific message content
             if "Transcript Part 5/8 Summary" in message['content']:
                 print(f"{message['role']}: {message['content']}")
-    print(f"-------End of Run {run+1}/{5}-------")
+    print(f"-------End of Run {run + 1}/{5}-------")
 
 # Print stored summaries
 print("\nStored summaries:")
